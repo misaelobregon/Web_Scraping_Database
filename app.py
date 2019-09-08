f801ea44da0578ae312161dcd12aa26580341a4e
misaelobregon <43828674+misaelobregon@users.noreply.github.com>
2019-09-08 14:45:38 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -13,22 +13,24 @@ mongo=PyMongo(app)
 
 @app.route("/")
 def index():
-    scraped_miss_mars=mongo.db.scraped_mars.find_one()
-    return render_template("index.html", scraped_miss_mars=scraped_miss_mars)
+    mars_data=mongo.db.scraped_mars.find_one()
+    return render_template("index.html", mars_data=mars_data)
 
 @app.route("/scrape")
-def scraper():
-    scraped_miss_mars=mongo.db.scraped_miss_mars
+def scrape():
+    
+    # Run the scrape function
+    mars_data=mongo.db.scraped_mars
     
     headlines=scrape_mars.news_scrape()
     images=scrape_mars.image_scrape()          
     tweets=scrape_mars.tweet_scrape()
     facts=scrape_mars.table_scrape()
     hemispheres=scrape_mars.hemisphere_img_scrape()
-    updates={"headlines":headlines,"images":images,"tweets":tweets,"facts":facts,"hemispheres":hemispheres}
+    update_data={"headlines":headlines,"images":images,"tweets":tweets,"facts":facts,"hemispheres":hemispheres}
 
-    scraped_miss_mars.update({}, updates, upsert=True)
+    mars_data.update({}, update_data, upsert=True)
     return redirect("/", code=302)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
